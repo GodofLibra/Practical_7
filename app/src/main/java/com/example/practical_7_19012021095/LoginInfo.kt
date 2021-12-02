161964bcc189d84d210de5ae8577b733706e5326
harshilshah334@gmail.com <harshilshah1110>
2021-12-02 14:06:28 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/practical_7_19012021095/LoginInfo.kt
@@ -0,0 +1,13 @@
+package com.example.practical_7_19012021095
+
+
+class LoginInfo {
+    companion object{
+        var name:String = ""
+        var phone:String = ""
+        var city: String = ""
+        var email:String = ""
+        var password:String = ""
+        var status:String = ""
+    }
+}
